25b9d58d3e423abf738425375022b02d157757b2
Cory Dominguez <corydominguez@gmail.com>
2019-04-17 19:12:21 -0700

    logic split for meta commands and statements

--- main.go
+++ main.go
@@ -4,7 +4,55 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type MetaCommand string
+
+const (
+	Exit MetaCommand = ".exit\n"
+	Help MetaCommand = ".help\n"
+)
+
+type QueryType int
+
+const (
+	Insert QueryType = iota
+	Select
+)
+
+type Statement struct {
+	querytype QueryType
+}
+
+func prepareStatement(input string) (statement Statement, err error) {
+	err = nil
+	if strings.HasPrefix(input, "insert") {
+		statement.querytype = Insert
+	} else if strings.HasPrefix(input, "select") {
+		statement.querytype = Select
+	} else {
+		end := 10
+		if len(input) < 10 {
+			end = len(input)
+		}
+		err = fmt.Errorf("Unrecognized query type for statement: %s", input[:end])
+	}
+	return
+
+}
+
+func executeStatement(statement Statement) error {
+	switch statement.querytype {
+	case Insert:
+		fmt.Print("This is where we would do an insert.\n")
+	case Select:
+		fmt.Print("This is where we would do a select.\n")
+	default:
+		return nil
+	}
+	return nil
+}
 
 func main() {
 	fmt.Print("Let's Go Clone SQLite3 version 0.0.1\n")
@@ -18,12 +66,27 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		if input == ".help\n" {
-			fmt.Print("noop\n")
-		} else if input == ".exit\n" {
-			os.Exit(0)
+		if strings.HasPrefix(input, ".") {
+			switch meta_command := MetaCommand(input); meta_command {
+			case Help:
+				fmt.Print("noop\n")
+			case Exit:
+				os.Exit(0)
+			default:
+				fmt.Printf("Unrecognized meta command %s", input)
+			}
 		} else {
-			fmt.Printf("Unrecognized command %s", input)
+			statement, err := prepareStatement(input)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
+			fmt.Printf("statement: %+v\n", statement)
+			err = executeStatement(statement)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(1)
+			}
 		}
 	}
 }
